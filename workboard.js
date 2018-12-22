fc275ca2d267f302805f551f015e9d324e44105a
WinterCricket <hodhodhod2003@gmail.com>
2018-12-22 13:55:48 -0600

    move sum to select odd and chain filter and reduce inside the higher order function

--- workboard.js
+++ workboard.js
@@ -1,25 +1,31 @@
+function sumOddFib(num) {
+
+	var list = fibsUpToSum(num);
+	
+	return list.filter(function(number){return number % 2 === 1;}).reduce(function(a, b){return a +b });
+};
 
 
-
-function fibsUpTo(num) {
+function fibsUpToSum(num) {
 
 	if(num === 1) return [1,1];
 	var list = [1,1];
 	while(true) {
 		var next = list[list.length -1] + list[list.length -2];
-		if(next <= num) {
+		if(next <= num  ) {
+
 			list.push(next);
 		} else {
-			return list.reduce(function(a, b){return a +b });
+			return list;
 		}
 
 
-		
-		
+
+
 
 	}
 }
-document.getElementById("demo").innerHTML=fibsUpTo(8);
+document.getElementById("demo").innerHTML=sumOddFib(8);
 
 
 
