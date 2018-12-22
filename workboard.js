ff3fd097cfb5e2318d220c5b7890c9aea17c8387
WinterCricket <hodhodhod2003@gmail.com>
2018-12-22 13:01:37 -0600

    start a create function seperate now from other func

--- workboard.js
+++ workboard.js
@@ -1,19 +1,25 @@
 
 
 
-function fibsUpTo(num) {
+// function fibsUpTo(num) {
 
-	if(num === 1) return [1,1];
-	var list = [1,1];
-	while(true) {
-		var next = list[list.length -1] + list[list.length -2];
-		if(next <= num) {
-			list.push(next);
-		} else {
-			return list;
-		}
-	}
+// 	if(num === 1) return [1,1];
+// 	var list = [1,1];
+// 	while(true) {
+// 		var next = list[list.length -1] + list[list.length -2];
+// 		if(next <= num) {
+// 			list.push(next);
+// 		} else {
+// 			return list;
+// 		}
+// 	}
+// }
+// document.getElementById("demo").innerHTML=fibsUpTo(23);
+
+
+function sumList(){
+	var list = [1,2,3,4,5];
+	return list.reduce(function(a, b){return a + b});
+	
 }
-document.getElementById("demo").innerHTML=fibsUpTo(23);
-
-
+document.getElementById("demo").innerHTML = sumList();
