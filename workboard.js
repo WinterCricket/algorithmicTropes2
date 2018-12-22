2aa22489c4b2837fc05e8a0ef35eccf8561b1eac
WinterCricket <hodhodhod2003@gmail.com>
2018-12-22 13:13:43 -0600

    merge the sum function into the fib with limit arr func

--- workboard.js
+++ workboard.js
@@ -1,25 +1,26 @@
 
 
 
-// function fibsUpTo(num) {
+function fibsUpTo(num) {
 
-// 	if(num === 1) return [1,1];
-// 	var list = [1,1];
-// 	while(true) {
-// 		var next = list[list.length -1] + list[list.length -2];
-// 		if(next <= num) {
-// 			list.push(next);
-// 		} else {
-// 			return list;
-// 		}
-// 	}
-// }
-// document.getElementById("demo").innerHTML=fibsUpTo(23);
+	if(num === 1) return [1,1];
+	var list = [1,1];
+	while(true) {
+		var next = list[list.length -1] + list[list.length -2];
+		if(next <= num) {
+			list.push(next);
+		} else {
+			return list.reduce(function(a, b){return a +b });
+		}
 
 
-function sumList(){
-	var list = [1,2,3,4,5];
-	return list.reduce(function(a, b){return a + b});
-	
+		
+		
+
+	}
 }
-document.getElementById("demo").innerHTML = sumList();
+document.getElementById("demo").innerHTML=fibsUpTo(8);
+
+
+
+// document.getElementById("demo").innerHTML = sumList();
