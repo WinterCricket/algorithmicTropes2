93db9b5f501f80a36f4c50a90f453eaf7d31e0b0
WinterCricket <hodhodhod2003@gmail.com>
2018-12-29 18:46:45 -0600

    create sorting of array from range

--- workboard.js
+++ workboard.js
@@ -1,50 +1,14 @@
 
 
 
-function sortArr(arr){
-	return takeOut(arr.sort());
+function smallestCommons(arr){
+	 arr.sort();
+	var array = [];
+	for(var i = arr[0]; i <= arr[arr.length -1]; i++){
+		array.push(i);
+	}
+	return array;
 }
-function takeOut(arr) {
-	var min = arr[0];
-  var max = arr[1];
-  var outOut = min + ", " +max;
-  return outOut;
-}
-sortArr([5, 1]);
-
-
-// function leastCommonMultiple(min, max) {
-	
-//     function range(min, max) {
-//         var array = [];
-//         for (var i = min; i <= max; i++) {
-//             array.push(i);
-//         }
-//         return array;
-//     }
-
-//     function gcd(a, b) {
-//         return !b ? a : gcd(b, a % b);
-//     }
-
-//     function lcm(a, b) {
-//         return (a * b) / gcd(a, b);   
-//     }
-
-//     var multiple = min;
-//     range(min, max).forEach(function(n) {
-//         multiple = lcm(multiple, n);
-//     });
-
-//     return multiple;
-// }
-// function myFunction(){
-// document.getElementById("demo").innerHTML= leastCommonMultiple(loHi);
-// }
-}
-
-
 function myFunction() {
-document.getElementById("demo").innerHTML = loHi;
+document.getElementById("demo").innerHTML = smallestCommons([8, 3]);
 }
-
