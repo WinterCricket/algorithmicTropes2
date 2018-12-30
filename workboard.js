1e6abe1d1f144b07ada10b603667139ee990c97d
WinterCricket <hodhodhod2003@gmail.com>
2018-12-30 13:38:50 -0600

    develop sort array tool

--- workboard.js
+++ workboard.js
@@ -1,46 +1,48 @@
 
 
 
-function smallestCommons(arr){
-	return takeOut(arr.sort());
-}
-function takeOut(arr) {
-	  var max = Math.max(arr[0],arr[1]);
-  var min = Math.min(arr[0],arr[1]);
+// function smallestCommons(arr){
+// 	return takeOut(arr.sort());
+// }
+// function takeOut(arr) {
+// 	  var max = Math.max(arr[0],arr[1]);
+//   var min = Math.min(arr[0],arr[1]);
   
-  return leastCommonMultiple(min, max);
-}
+//   return leastCommonMultiple(min, max);
+// }
 
 
 
-function leastCommonMultiple(min, max) {
+// function leastCommonMultiple(min, max) {
 	
-    function range(min, max) {
-        var array = [];
-        for (var i = min; i <= max; i++) {
-            array.push(i);
-        }
-        return array;
-    }
+//     function range(min, max) {
+//         var array = [];
+//         for (var i = min; i <= max; i++) {
+//             array.push(i);
+//         }
+//         return array;
+//     }
 
-    function gcd(a, b) {
-        return !b ? a : gcd(b, a % b);
-    }
+//     function gcd(a, b) {
+//         return !b ? a : gcd(b, a % b);
+//     }
 
-    function lcm(a, b) {
-        return (a * b) / gcd(a, b);   
-    }
+//     function lcm(a, b) {
+//         return (a * b) / gcd(a, b);   
+//     }
 
-    var multiple = min;
-    range(min, max).forEach(function(n) {
-        multiple = lcm(multiple, n);
-    });
+//     var multiple = min;
+//     range(min, max).forEach(function(n) {
+//         multiple = lcm(multiple, n);
+//     });
 
-    return multiple;
-}
+//     return multiple;
+// }
 
-
-
+var arr = [5, 1, 4, 9];
+ arr.sort();
+var o = arr[0];
 function myFunction() {
-document.getElementById("demo").innerHTML = smallestCommons([23, 18]);
+
+document.getElementById("demo").innerHTML = o;
 }
