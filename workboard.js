cef40ad65459ebb0cc6187e671139a9d055cac63
WinterCricket <hodhodhod2003@gmail.com>
2018-12-29 19:04:55 -0600

    use min max in for loop

--- workboard.js
+++ workboard.js
@@ -4,11 +4,11 @@
 function smallestCommons(arr){
 	 arr.sort();
 	var array = [];
-	for(var i = arr[0]; i <= arr[arr.length -1]; i++){
+	for (var i = Math.min(arr[0], arr[1]); i <= Math.max(arr[0], arr[1]); i++)﻿{
 		array.push(i);
 	}
 	return array;
 }
 function myFunction() {
-document.getElementById("demo").innerHTML = smallestCommons([8, 3]);
+document.getElementById("demo").innerHTML = smallestCommons([10, 2]);
 }
