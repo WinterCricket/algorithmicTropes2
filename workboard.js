e4470920b9352d008e04247e09bcd46222c17ba9
WinterCricket <hodhodhod2003@gmail.com>
2019-01-03 21:55:10 -0600

    try concat for steamrollArray

--- workboard.js
+++ workboard.js
@@ -1,66 +1,22 @@
 
-
-
-// function smallestCommons(arr){
-// 	return takeOut(arr.sort());
-// }
-// function takeOut(arr) {
-// 	  var max = Math.max(arr[0],arr[1]);
-//   var min = Math.min(arr[0],arr[1]);
-  
-//   return leastCommonMultiple(min, max);
-// }
+function steamrollArray(arr) {
+  // I'm a steamroller, baby
 
 
 
-// function leastCommonMultiple(min, max) {
-	
-//     function range(min, max) {
-//         var array = [];
-//         for (var i = min; i <= max; i++) {
-//             array.push(i);
-//         }
-//         return array;
-//     }
+    var ret = [];
+    for(var i = 0; i < arr.length; i++) {
+        if(Array.isArray(arr[i])) {
+            ret = ret.concat(steamrollArray[i]);
+        } else {
+            ret.push(arr[i]);
+        }
+    }
+    return ret;
 
-//     function gcd(a, b) {
-//         return !b ? a : gcd(b, a % b);
-//     }
-
-//     function lcm(a, b) {
-//         return (a * b) / gcd(a, b);   
-//     }
-
-//     var multiple = min;
-//     range(min, max).forEach(function(n) {
-//         multiple = lcm(multiple, n);
-//     });
-
-//     return multiple;
-// }
-
-// function dropElements(arr, func){
-	
-	
-//  arr.forEach(n => {
-//   return n; });
-
-// }
-// function myFunction() {
-
-// document.getElementById("demo").innerHTML = dropElements([1, 2, 3, 4], function(n) {return n < 3; });
-// }
-function dropElements(arr, func){
-
-//arr[0] to keep the right position for inspection as it shift due to shift, not n
-while(!func(arr[0])){
-	arr.shift();
-}
-return arr;
-}
-
+};
 
 
 function myFunction() {
-	document.getElementById("demo").innerHTML = dropElements([5, 1, 2, 3], function(n) {return n === 1;});
+	document.getElementById("demo").innerHTML = steamrollArray([1, [5], [3, [[4]]]]);
 }
