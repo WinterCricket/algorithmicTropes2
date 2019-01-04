e6992aba1bc024f336a2f97c013c5e31bc6fed5c
WinterCricket <hodhodhod2003@gmail.com>
2019-01-03 22:05:29 -0600

    refactored a bit

--- workboard.js
+++ workboard.js
@@ -1,22 +1,38 @@
 
+// function steamrollArray(arr) {
+//   // I'm a steamroller, baby
+
+
+
+//     var ret = [];
+//     for(var i = 0; i < arr.length; i++) {
+//         if(Array.isArray(arr[i])) {
+//             ret = ret.concat(steamrollArray[i]);
+//         } else {
+//             ret.push(arr[i]);
+//         }
+//     }
+//     return ret;
+
+// };
+
+
+// function myFunction() {
+// 	document.getElementById("demo").innerHTML = steamrollArray([1, [5], [3, [[4]]]]);
+// }
+
 function steamrollArray(arr) {
-  // I'm a steamroller, baby
-
-
-
     var ret = [];
     for(var i = 0; i < arr.length; i++) {
         if(Array.isArray(arr[i])) {
-            ret = ret.concat(steamrollArray[i]);
+            ret = ret.concat(steamrollArray(arr[i]));
         } else {
             ret.push(arr[i]);
         }
     }
     return ret;
+}
 
-};
-
-
-function myFunction() {
-	document.getElementById("demo").innerHTML = steamrollArray([1, [5], [3, [[4]]]]);
+function myFunction(){
+document.getElementById("demo").innerHTML = steamrollArray([1, [5], [3, [[4]]]]);
 }
