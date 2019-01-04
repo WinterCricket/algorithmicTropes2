9ac4eb923194d12e329a42dbe21e5bc97b1dc8a3
WinterCricket <hodhodhod2003@gmail.com>
2019-01-03 23:41:15 -0600

    use ArrayisArray and recursion and reduce method

--- workboard.js
+++ workboard.js
@@ -21,18 +21,17 @@
 // 	document.getElementById("demo").innerHTML = steamrollArray([1, [5], [3, [[4]]]]);
 // }
 
-function steamrollArray(arr) {
-    var ret = [];
-    for(var i = 0; i < arr.length; i++) {
-        if(Array.isArray(arr[i])) {
-            ret = ret.concat(steamrollArray(arr[i]));
-        } else {
-            ret.push(arr[i]);
-        }
-    }
-    return ret;
+function steamrollArray(arr){
+	return arr.reduce(function(acc, next){
+		if(Array.isArray(next)){
+			return steamrollArray(acc.concat(next));
+		}else{
+			return acc.concat(next);
+		}
+	}, []);
 }
 
+
 function myFunction(){
-document.getElementById("demo").innerHTML = steamrollArray([1, [5], [3, [[4]]]]);
+document.getElementById("demo").innerHTML = steamrollArray([[[[1, [2], [3, [[4]]]]]]]);
 }
