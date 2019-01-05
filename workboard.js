21666c45091180e6884e39b8e7c9c266bbfe604d
WinterCricket <hodhodhod2003@gmail.com>
2019-01-04 21:56:40 -0600

    translate binary string to text using regex

--- workboard.js
+++ workboard.js
@@ -1,37 +1,22 @@
 
-// function steamrollArray(arr) {
-//   // I'm a steamroller, baby
 
 
+function binaryAgent(str) {
+    // Removes the spaces from the binary string
+    str = str.replace(/\s+/g, '');
+    // Pretty (correct) print binary (add a space every 8 characters)
+    str = str.match(/.{1,8}/g).join(" ");
 
-//     var ret = [];
-//     for(var i = 0; i < arr.length; i++) {
-//         if(Array.isArray(arr[i])) {
-//             ret = ret.concat(steamrollArray[i]);
-//         } else {
-//             ret.push(arr[i]);
-//         }
-//     }
-//     return ret;
+    var newBinary = str.split(" ");
+    var binaryCode = [];
 
-// };
-
-
-// function myFunction() {
-// 	document.getElementById("demo").innerHTML = steamrollArray([1, [5], [3, [[4]]]]);
-// }
-
-function steamrollArray(arr){
-	return arr.reduce(function(acc, next){
-		if(Array.isArray(next)){
-			return steamrollArray(acc.concat(next));
-		}else{
-			return acc.concat(next);
-		}
-	}, []);
+    for (var i = 0; i < newBinary.length; i++) {
+        binaryCode.push(String.fromCharCode(parseInt(newBinary[i], 2)));
+    }
+    
+    return binaryCode.join("");
 }
 
-
 function myFunction(){
-document.getElementById("demo").innerHTML = steamrollArray([[[[1, [2], [3, [[4]]]]]]]);
+document.getElementById("demo").innerHTML = binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
 }
