4cbca0f97ef7ba053201d318858fc32c2c2289ed
WinterCricket <hodhodhod2003@gmail.com>
2019-01-05 23:42:28 -0600

    break down of array helpers but need to clarify or define a parameter

--- workboard.js
+++ workboard.js
@@ -1,14 +1,14 @@
 
 
 
- function truthCheck(collection, pre){
- 	
-var keysArray = Object.keys(collection.forEach(object => object));
+function truthCheck(collection, pre){
+var keysArray = collection.forEach(object => Object.keys(object));
 if(keysArray.every(pre)){
 	return true;
 }
 	return false;
 }
+
  function myFunction(){
-	document.getElementById("demo").innerHTML = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}]);
+	document.getElementById("demo").innerHTML = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
 }
