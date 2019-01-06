b735d6e8c3649129718429bd60899e4ebc31e8dc
WinterCricket <hodhodhod2003@gmail.com>
2019-01-06 14:39:05 -0600

    try some weird tinkering igorance to get a filter

--- workboard.js
+++ workboard.js
@@ -1,14 +1,31 @@
 
 
 
-function truthCheck(collection, pre){
-var keysArray = collection.forEach(object => Object.keys(object));
-if(keysArray.every(pre)){
-	return true;
-}
-	return false;
+// function truthCheck(collection, pre){
+// var keysArray = collection.forEach(object => Object.entries(object));
+// if(keysArray.every(pre)){
+// 	return true;
+// }
+// 	return false;
+// }
+
+//  function myFunction(){
+// 	document.getElementById("demo").innerHTML = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+// }
+var collection = [
+{"user": "Tinky-Winky", "sex": "male"}, 
+{"user": "Dipsy", "sex": "male"}, 
+{"user": "Laa-Laa", "sex": "female"}, 
+{"user": "Po", "tex": "female"}
+
+]
+var filteredCollection = [];
+for(var i = 0; i < collection.length; i++){
+	if(collection[i].sex){
+  	filteredCollection.push(collection[i]);
+
+  }
+  return filteredCollection.length === collection.length;
 }
 
- function myFunction(){
-	document.getElementById("demo").innerHTML = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
-}
+document.getElementById("demo").innerHTML = filteredCollection;
