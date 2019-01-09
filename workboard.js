5152c049541d0387d26b32e2daacdbb231011c13
WinterCricket <hodhodhod2003@gmail.com>
2019-01-08 21:19:59 -0600

    try another large function

--- workboard.js
+++ workboard.js
@@ -1,26 +1,27 @@
 
 
 
-// function truthCheck(collection, pre){
-// var keysArray = collection.forEach(object => Object.entries(object));
-// if(keysArray.every(pre)){
-// 	return true;
-// }
-// 	return false;
-// }
+function truthCheck(collection, pre){
+var keysArray = collection.forEach(object => Object.entries(object));
+if(keysArray.every(pre)){
+	return true;
+}
+	return false;
+}
 
-//  function myFunction(){
-// 	document.getElementById("demo").innerHTML = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
-// }
+ function myFunction(){
+	document.getElementById("demo").innerHTML = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+}
+
 function reject(array, iteratorFunction) {
 
     return array.filter(item => !array.filter(iteratorFunction).includes(item));
 
 }
 
-function myFunction(){
-document.getElementById("demo").innerHTML = reject([1, 3, 6, 7, 11, 12, 13], function(item){return item % 2});
-}
+// function myFunction(){
+// document.getElementById("demo").innerHTML = reject([1, 3, 6, 7, 11, 12, 13], function(item){return item % 2});
+// }
 
 
 
