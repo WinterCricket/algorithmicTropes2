31f5eb8eab634dcd9bedf338d386195d4eb715b5
WinterCricket <hodhodhod2003@gmail.com>
2019-01-23 00:21:23 -0600

    refactor for ternary

--- workboard.js
+++ workboard.js
@@ -299,3 +299,13 @@ function truthCheck(collection = "defaulted", pre = "defaulted") {
 	return collection.every(person => person === person.pre);
 }
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+
+//use ternary 
+function truthCheck(collection = "defaulted", pre = "defaulted") {
+ 
+  return collection.every(person=> person.find(pre) ? true : false);
+    
+}
+  
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
