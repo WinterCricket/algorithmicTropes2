6350681714661498ad3cba4b9064dd7d9fe023e5
WinterCricket <hodhodhod2003@gmail.com>
2019-01-31 16:56:16 -0600

    add literal palindrome func

--- workboard.js
+++ workboard.js
@@ -428,3 +428,16 @@ const companies = [
 const [one, ...rest] = companies;//output below
 // Shoe Stores
 // ["Google","Uber Devil Drive"]
+
+//most literal/direct palindrome func
+function palindrome(str) {
+ var re = /[^A-Za-z0-9]/g;
+ 
+ var len = str.length;
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
+}
