19c59da80519e99b29d768a4452831411d8069c1
WinterCricket <hodhodhod2003@gmail.com>
2019-02-13 16:51:23 -0600

    using map slice filter to chunk array into subarrays

--- workboard.js
+++ workboard.js
@@ -564,3 +564,13 @@ for(let i = 1; i <= n; i++){
 }
 
 }
+
+//chunking arrays
+function chunk(array, size) {
+
+
+return array.map( function(e,i){ 
+     return i%size===0 ? array.slice(i,i+size) : null; 
+}).filter(function(e){ return e; });
+}
+  chunk([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],5);
