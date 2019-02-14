58c3ac9df529060463a838824cb9a55066106276
WinterCricket <hodhodhod2003@gmail.com>
2019-02-13 21:34:17 -0600

    chunk no ternary but for of loop

--- workboard.js
+++ workboard.js
@@ -574,3 +574,18 @@ return array.map( function(e,i){
 }).filter(function(e){ return e; });
 }
   chunk([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],5);
+  //chunk array without ternary
+  function chunk(array, size){
+  const chunked = [];
+  for(let element of array){
+    const last = chunked[chunked.length -1];
+    if(!last || last.length === size){
+    chunked.push([element]);
+    }else {
+      last.push(element);
+    }
+  }
+  return chunked;
+}
+
+chunk([1,2,3,4,5,6,7,8,9,0], 3);
